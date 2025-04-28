cbc91ac65a9b561ef62450a6691abe9855bad765
hungdoba <hungdoba.hdb@gmail.com>
2025-04-28 10:08:18 +0900

    feat: update image configuration in next.config.ts to include formats and minimum cache TTL

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,8 @@ const nextConfig: NextConfig = {
     loader: 'custom',
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    formats: ['image/webp', 'image/avif'],
+    minimumCacheTTL: 60,
   },
   transpilePackages: ['next-image-export-optimizer'],
   env: {
